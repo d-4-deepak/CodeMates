b6a58304b67da9c969796db40c1d0d66f6a9a6a9
Deepak Gupta <dg934257@gmail.com>
2025-10-08 17:38:26 +0530

    multiple route handlers, next(), error associated

--- src/app.js
+++ src/app.js
@@ -1,28 +1,21 @@
 const express = require("express");
 
 const app = express();
-app.get("/ab*cd",(req,res)=>{
-    res.send("abcd")
-})
-app.get("/user",(req,res)=>{
-    res.send("get user")
-})
 
-app.get("/user/xyz",(req,res)=>{
-    res.send("get user/xyz")
-})
-app.post("/user",(req,res)=>{
-    res.send("post user")
-})
-app.patch("/user",(req,res)=>{
-    res.send("patch user")
-})
-app.put("/user",(req,res)=>{
-    res.send("put user")
-})
-app.delete("/user",(req,res)=>{
-    res.send("delete user")
-})
+app.use("/user",(req,res,next)=>{
+    //  res.send("1st route handler")
+    next();
+},
+(req,res,next)=>{
+    next()
+    // res.send("2nd route handler")
+},
+(req,res)=>{
+    console.log("djdj");
+    
+    res.send("3rd route handler")
+},
+)
 app.listen(7777,()=>{
     console.log("server is running successfully");
     
