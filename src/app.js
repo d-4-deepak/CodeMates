bb9ee2b59aed8041f89fcef2f15218c3985d1aa6
Deepak Gupta <dg934257@gmail.com>
2025-10-10 20:00:22 +0530

    signup api , feed api , user api

--- src/app.js
+++ src/app.js
@@ -4,20 +4,43 @@ const {connectDB} = require("./config/database")
 const User = require("./models/user")
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup",async (req,res)=>{
-    // creating a new instance of user model
-    const user = new User({
-        firstName: "Sachin",
-        lastName: "Tendulkar",
-        emailId: "st@gmail.com",
-        password: "sachin@123",
-    });
+    const user = new User(req.body);
    try{
     await user.save();
     res.send("user added succesfully!")
    }catch(err){
     res.status(400).send("Error saving the user: "+ err.message)
    }
+})
+
+app.get("/user",async (req,res)=>{
+    try{
+    const user =await User.findOne({emailId: req.body.emailId})
+    if(!user){
+        res.status(404).send("user not found")
+    }else{
+        res.send(user);
+    }
+
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+    try{
+      const users = await  User.find({});
+      if(users.length ===0){
+        res.status(404).send("user not found")
+      }else{
+        res.send(users)
+      }
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
 })
 
 connectDB().then(()=>{
